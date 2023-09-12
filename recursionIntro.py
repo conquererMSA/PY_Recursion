d26ca65c2f419d1c90b513cdccba3c8a4bc93fd7
140373331+conquererMSA@users.noreply.github.com <140373331+conquererMSA@users.noreply.github.com>
2023-09-12 14:19:38 +0600

    Factorial Recursion

--- recursionIntro.py
+++ recursionIntro.py
@@ -54,3 +54,51 @@ recursionlimit 6 er niche set kora zay na.
 #
 # recursive1(10) #10-(-1)
 # # recursive2(10) #10-1
+
+'''
+find factorial value of a number by recursion
+5! =5*4*3*2*1
+5! =5*4!
+   =5*4*3! =20*31!
+   =20*3! =20*3*2!
+   =60*2! =60*2*1!
+   =120*1! =120*1
+   =120, za 5 er factorial
+   **sob somoy previous number er value gulur factorial er sathe current number er 
+   factorial er value multiply korte hobe... zemon first number 5 er factorialValue 
+   hobe, 5*factorial(5-1) //5-1=4 za 0 theke boro
+         5*4*factorial(4-1) //4-1=3 za 0 theke boro
+         20*3*factorial(3-1) //3-1=2 za 0 theke boro
+         60*2*factorial(2-1) //2-1=1 za 0 theke boro
+         120*1*factorial(1-1) //1-1=0 za 0 er soman
+         120*factorial(0), factorial number 0 holei 1 return korbe ortath recursion 
+         bondho hoye zabe and factorial function output return korbe 1*120=120
+         
+   
+def factorial(n):      
+     if n==0:
+        return 1
+     return n*factorial(n-1)
+     
+factorialValue=factorial(5)
+print(factorialValue) #120
+'''
+def factorial(n):
+    # 5
+    # 4
+    # 3
+    # 2
+    # 1
+    # 0 holei stoping condition 1 return korbe
+    if n==1:
+        return 1
+    return n*factorial(n-1)  #return value
+#          5*factorial(5-1)
+#          20*factorial(4-1)
+#          60*factorial(3-1)
+#          120*factorial(2-1)
+#          return 120 factorial value
+
+factInput=int(input('Enter your number: '))
+factorialValue=factorial(factInput)
+print(factorialValue)
